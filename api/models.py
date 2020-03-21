a5d12a59cd8a09294b057a9d4514ac9e24c3fd87
2044smile <2044smile@naver.com>
2020-03-21 22:10:30 +0900

    practice prefetch_related

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    item_no = models.BigAutoField(primary_key=True)
+    item_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return str(self.item_no) + ' ' + str(self.item_name)
+
+
+class ItemImage(models.Model):
+    item_image_no = models.BigAutoField(primary_key=True)
+    item_no = models.ForeignKey(Item,
+                                related_name='related_item',
+                                on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=64)
+
+    def __str__(self):
+        return str(self.item_image_no) + ' ' + str(self.item_no) + ' ' + str(self.image_url)
